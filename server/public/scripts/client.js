cf20a040e63b515ceb9783981eae5695f371effe
TuckerLandis <landistuckerc@gmail.com>
2021-05-21 17:02:19 -0500

    bingo, post/get good

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,8 @@ console.log('JS');
 
 $(readyNow)
 
+let newObj = {};
+
 function readyNow() {
     console.log('JQ');
     
@@ -9,9 +11,13 @@ function readyNow() {
     $('#buttonPlus').on('click', clickPlus);
     $('#buttonSub').on('click', clickSub);
     $('#buttonMult').on('click', clickMult);
-    $('#buttonDiv').on('click', clickDiv);
+    $('#buttonDivide').on('click', clickDivide);
     $('#buttonSubmit').on('click', clickSubmit);
     $('#buttonClear').on('click', clickClear);
+
+    getHistory();
+    
+    let operator = {};
 
 }
 
@@ -21,32 +27,83 @@ function readyNow() {
 // on = press, send and clear inputs
 // on C press clear the inputs
 
+
+// on page refresh load (append history from server)
+
+
+/// possibilities for order - clicking submit sends an onbject || clicking submit adds an object to an array and also sends--too complicated maybe needed though
+
+
+
+function newObjSet(){
+    // make a new empty object on page load - tied to line 16
+    
+    newObj.num1 = $('#inputNum1').val();
+    newObj.num2 = $('#inputNum2').val();
+    newObj.operator = operator.operator;
+    
+}
+
 function clickPlus(){
     console.log('clicked plus');
-    
+    operator = {operator:'plus'};
 }
 
 function clickSub(){
     console.log('clicked sub');
+    operator = {operator:'sub'};
     
 }
 
 function clickMult(){
     console.log('clicked mult');
+    operator = {operator:'mult'};
     
 }
 
-function clickDiv(){
+function clickDivide(){
     console.log('clicked divide');
+    operator = {operator:'divide'};
     
 }
 
 function clickSubmit(){
     console.log('clicked submit');
-    
-}
+    newObjSet();
+    console.log(newObj);
+
+    $('#inputNum1').val('');
+    $('#inputNum2').val('');
+
+    // ajax post // on server receive and probably split to history - math functions 
+    $.ajax({
+        method: 'POST',
+        url: '/objCatcher',
+        data: newObj,
+    }).then(function (response) {
+        console.log(response);
+
+        getHistory();
+        
+}) //end then and ajax
+} // end clickSub
 
 function clickClear() {
     console.log('clicked clear');
+    $('#inputNum1').val('');
+    $('#inputNum2').val('');
+    
+}
+
+function getHistory(){
+    console.log('loading history');
+
+    $.ajax({
+        method: 'GET',
+        url: '/mathList'
+    }).then(function (response){
+        console.log(response);
+        
+    })
     
 }
--- server/server.js
+++ server/server.js
@@ -8,3 +8,30 @@ app.listen(PORT, () => {
     console.log('RUNNING ON PORT:', PORT)
 });
 
+/// "Database"
+let mathList = [];
+
+
+
+
+/// POST ROUTES
+app.post('/objCatcher', (req, res) => {
+   
+    console.log('receieved', req.body);
+    mathList.push(req.body);
+    console.log('Adjusted mathList is', mathList);
+    
+
+    res.sendStatus(201)
+    
+});
+
+
+/// GET ROUTES
+
+app.get('/mathList', (req, res) => {
+    console.log('got to /mathList');
+
+    res.send(mathList);
+    // instead of getting from the math array and doing work on the client to display, could format it in the server?
+})
