1c8c80024b5d90a5b453340e04e14133d6b843e1
TuckerLandis <landistuckerc@gmail.com>
2021-05-24 11:41:05 -0500

    removed commented out server code

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-// const PORT = process.env.PORT || 80
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-//app.listen(PORT, () => {
-//    console.log('RUNNING ON PORT:', PORT)
-//});
+app.use('/favicon.ico', express.static('server/public/images/favicon.ico'))
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
